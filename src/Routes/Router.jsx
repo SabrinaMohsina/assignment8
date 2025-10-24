0c1d3416de3a7f0f4761ca70d9a4c9b8a0d5a576
Sabrina Sultana <sabrinasultana381@gmail.com>
2025-10-24 23:51:33 +0600

    5

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
-    errorElement : <ErrorPage></ErrorPage>,
+    
     hydrateFallbackElement : <p>Loading ...</p>,
     children:[
         {
